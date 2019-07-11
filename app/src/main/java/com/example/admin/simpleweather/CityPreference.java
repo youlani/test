7af3b3653475cd4068b849fd8401f0ce9c20fe3f
youlani <940135763@qq.com>
2019-07-11 09:25:23 +0800

    2019/7/11

---
+++ app/src/main/java/com/example/admin/simpleweather/CityPreference.java
@@ -0,0 +1,19 @@
+package com.example.admin.simpleweather;
+
+        import android.app.Activity;
+        import android.content.SharedPreferences;
+
+public class CityPreference {
+    SharedPreferences prefs;
+    public CityPreference(Activity activity){
+        prefs=activity.getPreferences(Activity.MODE_PRIVATE);
+
+    }
+    String getCity(){
+        return prefs.getString("city", "HangZhou, CN");
+    }
+    void  setCity(String  city)
+    {
+        prefs.edit().putString("city",city).commit();
+    }
+}
---
+++ app/src/main/java/com/example/admin/simpleweather/RemoteFetch.java
@@ -0,0 +1,47 @@
+package com.example.admin.simpleweather;
+
+import android.content.Context;
+import android.net.wifi.aware.PublishConfig;
+
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class RemoteFetch {
+    private static final String OPEN_WEATHER_MAP_API=
+            "https://samples.openweathermap.org/data/2.5/weather?id=1808926&appid=b6907d289e10d714a6e88b30761fae22";
+    public static JSONObject getJSON(Context context,String city){
+        try {
+            URL url = new URL(String.format(OPEN_WEATHER_MAP_API, city));
+            HttpURLConnection connection =
+                    (HttpURLConnection)url.openConnection();
+
+            connection.addRequestProperty("x-api-key",
+                    context.getString(R.string.open_weather_maps_app_id));
+
+            BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(connection.getInputStream()));
+
+            StringBuffer json = new StringBuffer(1024);
+            String tmp="";
+            while((tmp=reader.readLine())!=null)
+                json.append(tmp).append("\n");
+            reader.close();
+
+            JSONObject data = new JSONObject(json.toString());
+
+            // This value will be 404 if the request was not
+            // successful
+            if(data.getInt("cod") != 200){
+                return null;
+            }
+
+            return data;
+        }catch(Exception e){
+            return null;
+        }
+    }
+}
